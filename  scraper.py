af8fed9240192b5a1ddbd0dbfb23fb3fd69c88ea
muhammadqasemtarboush1 <muhammadtarboush1@gmail.com>
2022-07-06 03:12:56 +0300

    update: readme & content of scraping

---  scraper.py
+++  scraper.py
@@ -1,30 +1,37 @@
 import requests
 from bs4 import BeautifulSoup
 
-
-# Create function named get_citations_needed_count
-# takes in a url string and returns an integer.
+url = "https://en.wikipedia.org/wiki/History_of_Mexico"
 
 
-# Create function named get_citations_needed_report
-# takes in a url string and returns a report string
-# the string should be formatted with each citation listed in the order found.
+def get_citations_needed_count(url):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    contents = soup.find_all('p')
+    counter = 0
+    for p_content in contents:
+        s_content = p_content.find_all('span')
+        if s_content:
+            counter += 1
+    return str(counter) + " the count of them"
 
 
+def get_citations_needed_report(url):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    contents = soup.find_all('p')
+    new_contents = ''
+    s_content = ''
+    counter = 0
+    for p_content in contents:
+        s_content = p_content.find_all('span')
+        if s_content:
+            counter += 1
+            new_contents += p_content.contents[0].strip() + '\n'
+    # if len(s_content) > 1:
+    return new_contents
 
-url = 'https://en.wikipedia.org/wiki/History_of_Mexico'
-r = requests.get(url)
 
-soup = BeautifulSoup(r.content , 'html.parser')
-
-jobs_cards = soup.find_all('p','sub')
-
-all_jobs = []
-
-for job in jobs_cards:
-    job
-    print(job,"ededede")
-
-
-job_titles = soup.find_all('h2', class_ = 'jb-title')
-print(job_titles)
+if __name__ == '__main__':
+    print(get_citations_needed_report(url))
+    print(get_citations_needed_count(url))
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # web-scraper
+
+## Run the Code:
+> 
+> python '.\ scraper.py'
+
+--- 
+
+Expected values:
+> get_citations_needed_count: 
+> 
+> returned values: 3 the count of them
+> 
+>  get_citations_needed_report: 
+> 
+> returned values: 
+> 
+> * The first people to settle in Mexico encountered a climate far milder than the current one. In particular, the Valley of Mexico contained several large paleo-lakes (known collectively as
+> * The Mexica people arrived in the Valley of Mexico in 1248 AD. They had migrated from the deserts north of the Rio Grande
+> * The Spanish had no intention to turn over Tenochtitlan to the Tlaxcalteca. While Tlaxcalteca troops continued to help the Spaniards, and Tlaxcala received better treatment than other indigenous nations, the Spanish eventually disowned the treaty. Forty years after the conquest, the Tlaxcalteca had to pay the same tax as any other indigenous community.
